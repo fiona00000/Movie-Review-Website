6e691df2c4a95c69b8f2c0f23b61755c8f893d42
Fiona <fionaloy000@gmail.com>
2023-08-10 18:02:40 -0400

    Uploading Interface modules

---
+++ MovieClient/src/components/notFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            Oops, This page could not be found!
+        </div>
+    )
+}
+
+export default NotFound
